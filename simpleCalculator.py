a725c0f8052a2b29a52c7bcb25dc179d8ded7924
hayden <haydenmcameron@proton.me>
2025-05-26 02:04:16 -0400

    simple calculator with +,-,x,/ functionality

---
+++ simpleCalculator.py
@@ -0,0 +1,38 @@
+print("Welcome to Simple Calculator")
+
+    
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+while True:
+    num1 = int(input("Enter first number:"))
+    operation = input("Enter operation:")[0]
+    num2 = int(input("Enter second number:"))
+
+    if(operation == '+'):
+        print('Result = ', add(num1,num2))
+    elif(operation == '-'):
+        print('Result = ', sub(num1,num2))
+    elif(operation == 'x'):
+        print('Result = ', multiply(num1,num2))
+    elif(operation == '/'):
+        print('Result = ', divide(num1,num2))
+    else:
+        print('Operation not recognized')
+
+
+        
+
+
+
+
+    
